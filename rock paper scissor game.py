a71de62a760b171b0c61eaa8b858fab91e152e70
PEDDAKOTLARAHUL <98511306+PEDDAKOTLARAHUL@users.noreply.github.com>
2023-05-18 10:31:51 +0530

    Add files via upload

---
+++ rock paper scissor game.py
@@ -0,0 +1,26 @@
+import random
+
+def play_game():
+    choices = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(choices)
+
+    while True:
+        user_choice = input("Enter your choice (rock/paper/scissors): ").lower()
+        if user_choice in choices:
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+    print(f"You choose {user_choice}.")
+    print(f"The computer choose {computer_choice}.")
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+            (user_choice == 'paper' and computer_choice == 'rock') or \
+            (user_choice == 'scissors' and computer_choice == 'paper'):
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+play_game()
